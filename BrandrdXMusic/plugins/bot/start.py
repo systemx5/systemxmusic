3b554736f5dece8d5d0acad5a77e8b46aba2ae79
systemx5 <154720925+systemx5@users.noreply.github.com>
2023-12-24 23:10:38 +0530

    Update start.py

--- BrandrdXMusic/plugins/bot/start.py
+++ BrandrdXMusic/plugins/bot/start.py
@@ -33,7 +33,7 @@ async def start_pm(client, message: Message, _):
         if name[0:4] == "help":
             keyboard = help_pannel(_)
             return await message.reply_video(
-                       video="https://graph.org/file/8fe2b8564471a468f4d19.mp4",
+                       video="https://te.legra.ph/file/74f4e28b5b7b7f7febd43.mp4",
                        caption=_["help_1"].format(config.SUPPORT_CHAT), reply_markup=keyboard
             )
         if name[0:3] == "sud":
