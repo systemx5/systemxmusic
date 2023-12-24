e471e7f28857dd764fed30ae758e03728d52a630
systemx5 <154720925+systemx5@users.noreply.github.com>
2023-12-24 16:04:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -86,7 +86,7 @@ START_IMG_URL = getenv(
     "START_IMG_URL", "https://te.legra.ph/file/5ed99a324f22d636d6305.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/68896b9d5b97252c68f64.jpg"
+    "PING_IMG_URL", "https://te.legra.ph/file/4d4a228cd374d65001dc1.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/568b292063504617f5adf.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/a2939756cee46fcb86e91.jpg"
